2e84f1bcd8b5262cb2042b434a53ee9b49b29f6a
Salvador <A01029956@ITESM.mx>
2021-09-16 20:31:31 -0500

    Marked excercise 1 as done.

--- Python/snake.py
+++ Python/snake.py
@@ -2,7 +2,7 @@
 
 Exercises
 
-1. How do you make the snake faster or slower?
+1. How do you make the snake faster or slower? [DONE]
 2. How can you make the snake go around the edges?
 3. How would you move the food?
 4. Change the snake to respond to arrow keys.
