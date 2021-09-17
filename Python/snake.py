7bd4a9a14b26807531595c7b46d43cd4c31ea393
Salvador <A01029956@ITESM.mx>
2021-09-16 22:12:35 -0500

    Changed status

--- Python/snake.py
+++ Python/snake.py
@@ -6,7 +6,7 @@ Exercises
 2. How can you make the snake go around the edges?
 3. How would you move the food? [ALREADY SATISFIED]
 4. Change the snake to respond to arrow keys. [ALREADY SATISFIED]
-*5. Make snake and fruit be different colors for every new game. []
+*5. Make snake and fruit be different colors for every new game. [DONE]
 """
 
 from turtle import *
